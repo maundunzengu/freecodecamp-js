89f8e57604484c336cff5ed1dbd73c2e97276de4
maundunzengu <isaacnzengu@gmail.com>
2026-05-08 14:57:15 +0300

    feat: created sentence_maker

---
+++ labs/sentence_maker.js
@@ -0,0 +1,24 @@
+let adjective = "red";
+let noun = "lion";
+let verb = "hunting";
+let place = "den";
+let adjective2 = "brown";
+let noun2 = "meat";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " " + "who loved to eat " + noun2 + "." + " The " + noun + " "  + "lived in a " + place + " " + "and had " + adjective2 + " " + "nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+
+let secondStory;
+adjective = "white";
+noun = "fox";
+verb = "playing";
+place = "forest";
+adjective2 = "black";
+noun2 = "flesh";
+
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " " + "who loved to eat " + noun2 + "." + " The " + noun + " " + "lived in a " + place + " " + "and had " + adjective2 + " " + "nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
+
+
+
