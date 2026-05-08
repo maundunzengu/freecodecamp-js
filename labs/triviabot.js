e2fb01f64c94c3963711442b0bd548949a480e51
maundunzengu <isaacnzengu@gmail.com>
2026-05-08 14:31:52 +0300

    feat:created trivia bot

---
+++ labs/triviabot.js
@@ -0,0 +1,18 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "bot's name";
+let botLocation = "where it's from";
+let favoriteLanguage = "its favorite coding language";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "It is quite hard to understand " + favoriteLanguage + ".";
+console.log(codingFact);
+
+codingFact = "Sometimes easy enough to translate " + favoriteLanguage + ".";
+console.log(codingFact);
+
+codingFact = "Often challenging " + favoriteLanguage + ".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
+
