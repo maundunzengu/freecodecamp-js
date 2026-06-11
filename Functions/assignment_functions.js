e39eeaa125e8b5313dbfef9d9a8f917ea610399a
maundunzengu <isaacnzengu@gmail.com>
2026-06-11 16:15:46 +0300

    feat: Functions

---
+++ Functions/assignment_functions.js
@@ -0,0 +1,49 @@
+function greet(name) {
+	return `Hello ${name}`;
+}
+
+console.log(greet("Isaac"));
+
+
+
+
+
+function add(a, b) {
+	return a + b;
+}
+
+console.log(add(5, -7));
+
+
+
+
+
+function isEvens(number) {
+	return number % 2 === 0;
+}
+
+console.log(isEvens(5));
+console.log(isEvens(30));
+
+
+const isEven = number => number % 2 === 2;
+console.log(isEven(20));
+
+
+function celsiusToFahrenheit (c) {
+	return (c * 9/5) + 32;
+}
+
+console.log(celsiusToFahrenheit(0));
+console.log(celsiusToFahrenheit(100));
+
+
+function introduce(name, age = 18) {
+	return `My name is ${name} and I am ${age} years old.`
+}
+
+console.log(introduce("Isaac"));
+
+
+const multiply = (a, b) => a * b;
+console.log(multiply(4, 5));
