27dc18ac9443138b48de26a93050d9f4bd6c4c44
maundunzengu <isaacnzengu@gmail.com>
2026-06-18 20:45:41 +0300

    added a new file

---
+++ labs/example_arrays.js
@@ -0,0 +1,10 @@
+let fruits = ["apple", "banana", "orange", "banana"];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits.indexOf("banana", 0));
+
+
+console.log(fruits.indexOf("banana", 2));
+
+
+console.log(fruits.length);
